65e35a7e395e49f5c70346dc32a716348f4f6559
bezkot1508 <daminovbekzod13@gmail.com>
2024-02-18 12:35:33 +0500

    added active img on thambnail click

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+const Modifier = {
+    imgShowcaseThumbnailActive : 'img-showcase__thumbnail-active'
+}
 
+
+// SHOPPING CART 
 const elSiteHeaderCartLink = document.querySelector(".js-site-header__cart-link");
 const elSiteHeaderCartModal = document.querySelector(".site-header__cart-modal")
-
 
 if (elSiteHeaderCartLink) {
     elSiteHeaderCartLink.addEventListener('click', function (e) {
@@ -11,6 +15,28 @@ if (elSiteHeaderCartLink) {
     })
 }
 
+
+// showcase button 
+const elImgShowcaseActiveImg = document.querySelector(".img-showcase__active-img")
+const elsImgShowcaseThubmnailButton = document.querySelectorAll(".js-img-showcase__thubmnail-button")
+const elsImgShowcaseThumbnail = document.querySelectorAll(".img-showcase__thumbnail")
+
+elsImgShowcaseThubmnailButton.forEach(function (elButton) {
+    elButton.addEventListener('click', function () {
+        
+        elsImgShowcaseThumbnail.forEach(function (elThumb) {
+            // remove active 
+            elThumb.classList.remove(Modifier.imgShowcaseThumbnailActive)
+        })
+        elButton.parentElement.classList.add(Modifier.imgShowcaseThumbnailActive)
+        elImgShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig;
+        elImgShowcaseActiveImg.srcset = `${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseRetina} 2x`;
+
+        console.log(elImgShowcaseActiveImg.src);
+        console.log(elButton.dataset.imgShowcaseBig);
+        console.log(elButton.dataset.imgShowcaseRetina);
+    })
+})
 
 // const elIcon = document.querySelector('.site-header__card-icon')
 // const elCart = document.querySelector('.site-header__cart-modal')
